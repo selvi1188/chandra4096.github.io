05df5985a482b21bf11ba63f2c47c40ab077c56f
selvi1188 <157682030+selvi1188@users.noreply.github.com>
2025-05-25 21:16:00 +0700

    Add files via upload

---
+++ edit.js
@@ -0,0 +1,120 @@
+window.addEventListener('scroll', function () {
+    var element = this.document.querySelector('.hh1')
+    var position = element.getBoundingClientRect()
+
+    if (position.top < window.innerHeight && position.bottom >= 0) {
+        element.classList.add('visible')
+    } else {
+        element.classList.remove('visible')
+    }
+})
+
+window.addEventListener('scroll', function () {
+    var element = this.document.querySelector('.hh21')
+    var position = element.getBoundingClientRect()
+
+    if (position.top < window.innerHeight && position.bottom >= 0) {
+        element.classList.add('visible')
+    } else {
+        element.classList.remove('visible')
+    }
+})
+
+window.addEventListener('scroll', function () {
+    var element = this.document.querySelector('.hh22')
+    var position = element.getBoundingClientRect()
+
+    if (position.top < window.innerHeight && position.bottom >= 0) {
+        element.classList.add('visible')
+    } else {
+        element.classList.remove('visible')
+    }
+})
+
+window.addEventListener('scroll', function () {
+    var element = this.document.querySelector('.hh3')
+    var position = element.getBoundingClientRect()
+
+    if (position.top < window.innerHeight && position.bottom >= 0) {
+        element.classList.add('visible')
+    } else {
+        element.classList.remove('visible')
+    }
+})
+
+window.addEventListener('scroll', function () {
+    var element = this.document.querySelector('.abt')
+    var position = element.getBoundingClientRect()
+
+    if (position.top < window.innerHeight && position.bottom >= 0) {
+        element.classList.add('visible')
+    } else {
+        element.classList.remove('visible')
+    }
+})
+
+window.addEventListener('scroll', function () {
+    var element = this.document.querySelector('.about-img img')
+    var position = element.getBoundingClientRect()
+
+    if (position.top < window.innerHeight && position.bottom >= 0) {
+        element.classList.add('visible')
+    } else {
+        element.classList.remove('visible')
+    }
+})
+
+window.addEventListener('scroll', function () {
+    var element = this.document.querySelector('.service')
+    var position = element.getBoundingClientRect()
+
+    if (position.top < window.innerHeight && position.bottom >= 0) {
+        element.classList.add('visible')
+    } else {
+        element.classList.remove('visible')
+    }
+})
+
+window.addEventListener('scroll', function () {
+    var element = this.document.querySelector('.know')
+    var position = element.getBoundingClientRect()
+
+    if (position.top < window.innerHeight && position.bottom >= 0) {
+        element.classList.add('visible')
+    } else {
+        element.classList.remove('visible')
+    }
+})
+
+window.addEventListener('scroll', function () {
+    var element = this.document.querySelector('#news')
+    var position = element.getBoundingClientRect()
+
+    if (position.top < window.innerHeight && position.bottom >= 0) {
+        element.classList.add('visible')
+    } else {
+        element.classList.remove('visible')
+    }
+})
+
+window.addEventListener('scroll', function () {
+    var element = this.document.querySelector('.ac')
+    var position = element.getBoundingClientRect()
+
+    if (position.top < window.innerHeight && position.bottom >= 0) {
+        element.classList.add('visible')
+    } else {
+        element.classList.remove('visible')
+    }
+})
+
+window.addEventListener('scroll', function () {
+    var element = this.document.querySelector('.fig')
+    var position = element.getBoundingClientRect()
+
+    if (position.top < window.innerHeight && position.bottom >= 0) {
+        element.classList.add('visible')
+    } else {
+        element.classList.remove('visible')
+    }
+})
